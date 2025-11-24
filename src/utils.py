aef8e21a65acc806c760f21987f9c11d64cd99c1
sathyalhr143 <sathyalhr143@gmail.com>
2025-11-24 14:01:51 -0500

    utils

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,8 @@ import os
 import sys
 import pandas as pd
 import numpy as np
-import pickle
-# import dill
+
+import dill
 
 from src.exception import CustomException
 from src.logger import logging
@@ -19,7 +19,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
         
         with open(file_path, 'wb') as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
             
     except Exception as e:
         raise CustomException(e, sys)
