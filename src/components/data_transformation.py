cfbcaf6799fd8d2b3ee095cb9a24feda034bee90
sathyalhr143 <sathyalhr143@gmail.com>
2025-11-24 01:33:44 -0500

    data_transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -8,6 +8,7 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
 
 @dataclass
 class DataTransformationConfig:
@@ -18,17 +19,22 @@ class DataTransformation:
         self.data_transformation_config = DataTransformationConfig()
         
     def get_data_transformer_object(self):
+        """This function does data transformation"""
+        
         try:
             numerical_columns = ['writing_score', 'reading_score']
             categorical_columns = ['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch',
     'test_preparation_course', 'reading_score', 'writing_score']
             num_pipeline = Pipeline(
                 steps=[
+                    ("imputer",SimpleImputer(strategy='median')),
                     ('scaler', StandardScaler())
+                    
                 ]
             )
             cat_pipeline = Pipeline(
                 steps=[
+                    ("imputer",SimpleImputer(strategy='most_frequent')),
                     ('one_hot_encoder', OneHotEncoder()),
                     ('scaler', StandardScaler(with_mean=False))
                 ]
@@ -43,6 +49,36 @@ class DataTransformation:
                 ]
             )
             return preprocessor
+            
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+        
+    def initiate_data_transformation(self, train_path, test_path):
+        
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)    
+            
+            logging.info("Read train and test data completed")
+            
+            logging.info("Obtaining preprocessor object")
+            preprocessor_obj = self.get_data_transformer_object()
+            
+            target_column_name = 'math_score'
+            numerical_columns = ['writing_score', 'reading_score']
+            
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+            
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+            
+            
+            logging.info(f"Train dataframe head : \n{train_df.head().to_string()}")
+            logging.info(f"Test dataframe head : \n{test_df.head().to_string()}")
+            
+            
             
         except Exception as e:
             raise CustomException(e, sys)
